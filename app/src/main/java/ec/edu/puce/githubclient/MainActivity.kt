ddf3106588130bd1762b7d499c2abddd34f4ed78
Stefojc14 <stefojc14sc06@hotmail.com>
2026-05-29 16:28:49 -0500

    Laboratorio 3.2

--- app/src/main/java/ec/edu/puce/githubclient/MainActivity.kt
+++ app/src/main/java/ec/edu/puce/githubclient/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import ec.edu.puce.githubclient.ui.screens.RepoForm
 import ec.edu.puce.githubclient.ui.screens.RepoList
 import ec.edu.puce.githubclient.ui.theme.GithubClientTheme
 
@@ -20,28 +18,16 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GithubClientTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    RepoList(
-                        modifier = Modifier.padding(paddingValues = innerPadding)
+                var currentScreen by remember { mutableStateOf("repoList") }
+                when (currentScreen) {
+                    "repoList" -> RepoList (
+                        onNavigateToForm = { currentScreen = "repoForm"}
+                    )
+                    "repoForm" -> RepoForm(
+                        onBackClick = { currentScreen = "repoList" }
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GithubClientTheme {
-        Greeting("Android")
     }
 }
